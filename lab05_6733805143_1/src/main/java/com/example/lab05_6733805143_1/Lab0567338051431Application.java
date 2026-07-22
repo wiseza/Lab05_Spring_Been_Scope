bf964d14f9c83fd0083c03c329f89dee5d70ac85
LynxelSins <wiphawi_ri@kkumail.com>
2026-07-22 14:09:32 +0700

    Create Spring Boot

---
+++ lab05_6733805143_1/src/main/java/com/example/lab05_6733805143_1/Lab0567338051431Application.java
@@ -0,0 +1,13 @@
+package com.example.lab05_6733805143_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab0567338051431Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab0567338051431Application.class, args);
+	}
+
+}
---
+++ lab05_6733805143_1/src/test/java/com/example/lab05_6733805143_1/Lab0567338051431ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab05_6733805143_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab0567338051431ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
