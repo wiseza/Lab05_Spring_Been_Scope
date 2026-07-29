0a2f3dad235f2655bbdd9e734e9c6c48d657fef8
wiseza <wiphawi_ri@kkumail.com>
2026-07-30 00:51:52 +0700

    Update CoffeeService.java

--- lab05_6733805143_1/src/main/java/com/example/lab05_6733805143_1/service/CoffeeService.java
+++ lab05_6733805143_1/src/main/java/com/example/lab05_6733805143_1/service/CoffeeService.java
@@ -1,19 +1,21 @@
 package com.example.lab05_6733805143_1.service;
 
-import com.example.coffeeshop.model.Coffee;
-import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.lab05_6733805143_1.model.Coffee;
 
 @Service
 public class CoffeeService {
 
     private List<Coffee> coffeeList = new ArrayList<>();
-    private int nextId = 3L;
+    private int nextId = 3;
 
     public CoffeeService() {
-        coffeeList.add(new Coffee(1L, "Espresso", 45));
-        coffeeList.add(new Coffee(2L, "Latte", 55));
+        new Coffee(1, "Espresso", 45);
+        new Coffee(2, "Latte", 55);
     }
 
     public List<Coffee> getAllCoffee() {
@@ -23,11 +25,11 @@ public class CoffeeService {
     public Coffee getCoffeeById(int id) {
 
         for (int i = 0; i < coffeeList.size(); i++) {
-            if (coffeeList.get(i).getId().equals(id)) {
+            if (coffeeList.get(i).getId() == id) {
                 return coffeeList.get(i);
             }
         }
-        return "ไม่พบข้อมูล";
+        return null;
     }
 
     public Coffee addCoffee(Coffee coffee) {
@@ -39,7 +41,7 @@ public class CoffeeService {
 
     public Coffee updateCoffee(int id, Coffee coffee) {
         for (int i = 0; i < coffeeList.size(); i++) {
-            if (coffeeList.get(i).getId().equals(id)) {
+            if (coffeeList.get(i).getId() == id) {
                 coffeeList.get(i).setName(coffee.getName());
                 coffeeList.get(i).setPrice(coffee.getPrice());
                 return coffeeList.get(i);
@@ -50,7 +52,7 @@ public class CoffeeService {
 
     public void deleteCoffee(int id) {
         for (int i = 0; i < coffeeList.size(); i++) {
-            if (coffeeList.get(i).getId().equals(id)) {
+            if (coffeeList.get(i).getId() == id) {
                 coffeeList.remove(i);
                 return;
             }
